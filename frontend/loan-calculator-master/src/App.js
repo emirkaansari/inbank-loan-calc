b30a01b0c2dd53ba1eae10ea718d16670e50f781
Emir Kaan Sarı <70165965+emirkaansari@users.noreply.github.com>
2023-03-28 00:17:33 +0300

    Add files via upload

---
+++ frontend/loan-calculator-master/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoanForm from './LoanForm';
+import './App.css'
+function App() {
+  return (
+    <div>
+    <h1>Loan Application Form</h1>
+      <LoanForm />
+    </div>
+  );
+}
+
+export default App;
+
